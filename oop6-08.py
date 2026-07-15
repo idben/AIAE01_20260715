b2a49229f1d1d4fed84a26a035b26896207fd61b
Ben Chen <idben.chen@gmail.com>
2026-07-15 15:17:15 +0800

    覆蓋 __repr__，定義 repr 出的內容

--- oop6-08.py
+++ oop6-08.py
@@ -6,6 +6,10 @@ class Point:
         self.y = y
     def __str__(self) -> str:
         return f"({self.x}, {self.y})"
+    def __repr__(self) -> str:
+        return f"Point(x={self.x} ,y={self.y})"
 
 p01 = Point(4, 3)
 print(p01)
+print(repr(p01))
+print([p01])
