e6398c1ee06030cb178a5c4fb0d781cb745dcf50
Ben Chen <idben.chen@gmail.com>
2026-07-15 15:10:32 +0800

    覆蓋 __str__，定義 print 出的內容

---
+++ oop6-08.py
@@ -0,0 +1,11 @@
+# 覆蓋 __str__，定義 print 出的內容
+
+class Point:
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+    def __str__(self) -> str:
+        return f"({self.x}, {self.y})"
+
+p01 = Point(4, 3)
+print(p01)
